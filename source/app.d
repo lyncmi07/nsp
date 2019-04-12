1acc382e7b418fc8ad9f2c2c4902f22f21f33368
Michael Lynch <lyncmi07@yahoo.co.uk>
2019-04-12 23:48:33 +0100

    checking modification times for files

--- source/app.d
+++ source/app.d
@@ -1,6 +1,41 @@
 import std.stdio;
+import std.file;
+import std.datetime.systime : SysTime, Clock;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  currentDirectory = getcwd();
+  long previousBuildTime = getPreviousBuildTime();
+  writeCurrentBuildTime();
+
+  writeln("Previous edit:", previousBuildTime);
+
+	writeln("currentDirectory:", currentDirectory);
+
+  foreach (DirEntry file; dirEntries(currentDirectory, SpanMode.depth)) {
+    long modTime = file.timeLastModified.stdTime;
+
+    string rebuild;
+    if (modTime < previousBuildTime) {
+      rebuild = "Shouldn't rebuild ";
+    } else {
+      rebuild = "Should rebuild ";
+    }
+    writeln(rebuild, file.name, " was modified at ", modTime);
+  }
+}
+
+long getPreviousBuildTime() {
+  if (getSize(buildTimeFile) == 0) return 0;
+
+  return (cast(const(long[]))read(buildTimeFile))[0];
+}
+void writeCurrentBuildTime() {
+  long currentBuildTime = Clock.currTime.stdTime;
+  std.file.write(buildTimeFile, [ currentBuildTime ]);
+}
+
+string currentDirectory = null;
+string buildTimeFile() {
+  return currentDirectory ~ "/.nsp/buildTime";
 }
